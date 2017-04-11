885599685b071555e915a6fab3deaa468aad76fd
Anna McGeachy <anna.mcgeachy@gmail.com>
2017-04-10 22:43:49 -0700

    did the first half of the exercises! but now I need sleep

---
+++ CME 193 - intro to scientific python/lecture1/ex1pt7.py
@@ -0,0 +1,7 @@
+name='Anna'
+print("Hello", name, "!")
+
+#weirdly, the learn python the hard way approach of 
+print("Hello %s!") % name 
+#doesn't seem to work 
+#because it seems to be trying to do a remainder calc on the strings. interesting. 
---
+++ CME 193 - intro to scientific python/lecture1/script1.py
@@ -0,0 +1,7 @@
+print(3 + 1)
+print(3 * 3)
+print(2 ** 3)
+print("Hello, world!")
+
+#running the script without the addition of the print() statement for each line returns nothing
+#because we're not telling it to explicitly print the result to the screen
